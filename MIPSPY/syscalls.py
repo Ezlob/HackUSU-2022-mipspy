501a0755a0ab582608a471cd46873ce10b988bb3
Ezlob <enyaburningham@gmail.com>
2022-03-18 20:05:23 -0600

    setting up syscalls

---
+++ MIPSPY/syscalls.py
@@ -0,0 +1,41 @@
+from locale import ABMON_10
+
+
+def syscall(v):
+    match v:
+        case 1 | 2 | 3 |4:
+           #print cases
+           if(Dict.get("v0") ) {
+               value = $a0
+               print(value)
+           } else if (Dict.get("f12"))
+                value = $f12
+                print(value)
+            return
+        case 5:
+
+            return
+        case 6:
+            return
+        case 7:
+            return
+        case 8:
+            return
+        case 9:
+            return
+        case 10:
+            return
+        case 11:
+            return
+        case 12:
+            return
+        case 13:
+            return
+        case 14:
+            return
+        case 15:
+            return
+        case 16:
+            return
+        case 17:
+            return
