4c53ae6d1e42b3cf89e3e0bbca9d05383d9abdef
BethanyChristensen <A02299515@aggies.usu.edu>
2022-03-19 07:06:10 -0600

    saving argparse progress

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 #! user/bin/python3
 
+import argparse
 from MIPSPY.mips import MIPS
 import sys
+import wrapper
+
+#run mode, debug mode
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', help = 'run the file in debug mode')
+args = parser.parse_args()
+
 
 # Check if program has file passed to it
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
     print("Make sure to pass in a file dumby")
     sys.exit()
 
