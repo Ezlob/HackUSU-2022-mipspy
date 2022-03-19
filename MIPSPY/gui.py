ddb46a4fcc66ae91846557b34be534b1b41a2b33
emmaMonty <montgomery.emma0710@gmail.com>
2022-03-18 23:53:51 -0600

    ice cream time

--- MIPSPY/gui.py
+++ MIPSPY/gui.py
@@ -1,1 +1,9 @@
-import curse
+import curses
+from curses import wrapper
+
+def main(stdscr):
+    stdscr.clear()
+    stdscr.addstr(0,20,"hello world")
+    stdscr.refresh()
+    stdscr.getch()
+wrapper(main)
