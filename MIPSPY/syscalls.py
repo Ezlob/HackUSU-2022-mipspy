9b13507fc464882f5e588c5593f9d3930cfdc26a
Ezlob <enyaburningham@gmail.com>
2022-03-18 22:06:41 -0600

    getting most syscalls done

--- MIPSPY/syscalls.py
+++ MIPSPY/syscalls.py
@@ -2,22 +2,24 @@ from mips import MIPS
 
 mips = MIPS()
 
+
 def syscall():
     match mips.registers.get("v0"):
         case 1 | 2 | 3 | 4:
-           #print cases
-           #a0 for string or int
-           if (mips.registers.get("a0") != 0) :
+            # print cases
+            # a0 for string or int
+            if (mips.registers.get("a0") != 0):
                 value = mips.registers.get("a0")
                 mips.registers["a0"] = 0
                 print(value)
-           #f12 for double or float 
-           elif (mips.registers.get("f12") != 0):
+            # f12 for double or float
+            elif (mips.registers.get("f12") != 0):
                 value = mips.registers.get("f12")
                 mips.registers["f12"] = 0
                 print(value)
+            return
         case 5:
-            #read_int
+            # read_int
             mips.registers["v0"] = int(input())
             return
         case 6 | 7:
@@ -25,41 +27,56 @@ def syscall():
             mips.registers["v0"] = float(input())
             return
         case 8:
-            #read_string
+            # read_string
             str_length = mips.registers.get("a1")
             input_str = input()
             if(len(input_str) > str_length):
                 print("Error! Too many characters in string")
-                sys.exit()
             else:
-                #TODO: set this to a memory location specified by the value of a0
-                #rather than putting it in a0
+                # TODO: set this to a memory location specified 
+                # by the value of a0
                 mips.registers["a0"] = input_str
-                return
+            return
         case 9:
-            #sbrk
+            # sbrk-allocate a certain amount of space in memory at address
+            mem_amount = mips.registers.get("a0")
+            address = int(input())
+            #TODO: figure it out :thumbsup:
             return
         case 10:
-            #exit
+            # exit
+            sys.exit()
             return
         case 11:
-            #print_character
+            # print_character
+            value = mips.registers.get("a0")
+            mips.registers["a0"] = 0
+            print(value)
             return
         case 12:
-            #read_character
+            # read_character
+            input_str = input()
+            if(len(input_str) > 1):
+                print("Error! Character can only be one byte")
+            else:
+                mips.registers["v0"] = input_str
             return
         case 13:
-            #open
+            # open
             return
         case 14:
-            #read
+            # read
             return
         case 15:
-            #write
+            # write
             return
         case 16:
-            #close
+            # close
+            file_to_close = mips.registers["a0"]
+            file_to_close.close()
             return
         case 17:
-            #exit2
+            # exit2
+            mips.registers["a0"] = input()
+            sys.exit()
             return
