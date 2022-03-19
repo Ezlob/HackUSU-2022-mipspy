2c03654c6c08e723b7aed89d96adf3fccfde26f9
BethanyChristensen <A02299515@aggies.usu.edu>
2022-03-19 08:48:25 -0600

    found error in argparser, updated main.py with fix

--- main.py
+++ main.py
@@ -9,25 +9,22 @@ from MIPSPY.mips_wrapper import debugger
 
 parser = argparse.ArgumentParser(description="Run MIPS files normally or use debug mode to traverse the file line by line")
 parser.add_argument("-d", "--debug",  help = 'run the file in debug mode', action = "store_true")
+parser.add_argument("file", type=str, help="the mips file")
 args = parser.parse_args()
 
 
 # Check if program has file passed to it
-if len(sys.argv) < 3:
+if len(sys.argv) < 2:
     print("Make sure to pass in a file dumby")
     sys.exit()
 
-
-file = sys.argv[1]
-
-# call mips emulator
-
-if sys.argv[2] == '-d':
-    m = debugger(file)
+#put into MIPS
+if len(sys.argv) > 2:
+    if sys.argv[2] == '-d':
+        m = debugger(args.file) #if we run in debug mode
 
 else:
-    m = MIPS(file)
-
+    m = MIPS(args.file) #default
 
 # this is what runs the program
 m.run()
