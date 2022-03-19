ad6a85aa6fae300e89627310ec2658d1c0948e01
Ezlob <enyaburningham@gmail.com>
2022-03-19 01:24:19 -0600

    adding to gui

--- MIPSPY/gui.py
+++ MIPSPY/gui.py
@@ -1,7 +1,16 @@
+from cmath import rect
 import curses
 from curses import wrapper
+from curses.textpad import rectangle
 
 def main(stdscr):
     stdscr.clear()
+    rectangle(stdscr, 0, 0, 38, 130)
+    rectangle(stdscr, 0, 0, 35, 75)
+    rectangle(stdscr, 0, 10, 17, 10)
+    stdscr.refresh()
+    stdscr.getch()
+
+
 
 wrapper(main)
