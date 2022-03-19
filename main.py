e2e161f3f5397ebcf5e6af6ddcdca22ce1e1404e
Dalyn Dalton <dalyn.dalton@usu.edu>
2022-03-19 11:02:45 -0600

    fixed up the main.py function, retiring the tester.py function

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
 #! user/bin/python3
 
 import argparse
+from pickle import TRUE
 from MIPSPY.mips import MIPS
 import sys
 from MIPSPY.mips_wrapper import debugger
 
-#run mode, debug mode
+# run mode, debug mode
 
-parser = argparse.ArgumentParser(description="Run MIPS files normally or use debug mode to traverse the file line by line")
-parser.add_argument("-d", "--debug",  help = 'run the file in debug mode', action = "store_true")
+parser = argparse.ArgumentParser(
+    description="Run MIPS files normally or use debug mode to traverse the file line by line"
+)
+parser.add_argument(
+    "-d", "--debug", help="run the file in debug mode", action="store_true"
+)
 parser.add_argument("file", type=str, help="the mips file")
 args = parser.parse_args()
 
+m: MIPS
+DEBUG = False
+
+# put into MIPS
+if "-d" in sys.argv:
+    sys.argv.remove("-d")
+    DEBUG = TRUE  # if we run in debug mode
 
 # Check if program has file passed to it
 if len(sys.argv) < 2:
     print("Make sure to pass in a file dumby")
     sys.exit()
 
-#put into MIPS
-if len(sys.argv) > 2:
-    if sys.argv[2] == '-d':
-        m = debugger(args.file) #if we run in debug mode
+# Check if program has file passed to it
+if len(sys.argv) < 2:
+    print("Make sure to pass in a file dumby")
+    sys.exit()
 
+# put into MIPS
+if DEBUG:
+    m = debugger(args.file)  # if we run in debug mode
+    m.debug_loop()
 else:
-    m = MIPS(args.file) #default
-
-# this is what runs the program
-m.run()
+    m = MIPS(args.file)  # default
+    m.run()
